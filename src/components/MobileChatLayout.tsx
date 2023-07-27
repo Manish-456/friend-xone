24c9c06891bdcc852278aea641d05cecfbba5985
Manish-456 <tamangmanish3456@gmail.com>
2023-07-27 11:11:55 +0545

    z-index added

--- src/components/MobileChatLayout.tsx
+++ src/components/MobileChatLayout.tsx
@@ -32,7 +32,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({ friends, session, sidebar
   }, [pathname])
 
   return (
-    <div className='fixed bg-zinc-50 border-b border-zinc-200 top-0 inset-x-0 py-2 px-4'>
+    <div className='fixed bg-zinc-50 border-b border-zinc-200 top-0 inset-x-0 py-2 z-[90] px-4'>
       <div className='w-full flex justify-between items-center'>
         <Link
           href='/dashboard'
